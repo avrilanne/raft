0365e84c55d5e29a3278d7e113e9e32dd7d9a6ad
Al Musavi & Sarah Dudek <pair+almusavi.sarahdudek@devbootcamp.com>
2017-03-19 14:56:09 -0500

    send mail

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,12 +3,33 @@ class InvitationsController < ApplicationController
   def new
   end
 
+  def index
+    @group = Group.find_by(id: params[:group_id])
+    if params[:search]
+      @users = User.search(params[:search])
+    else
+      @users = User.all
+    end
+  end
+
+  def show
+    @user = User.find_by(id: current_user.id)
+  end
+
   def create
-  p params
-  p current_user
-  @user = User.find_by(id: params[:user_id])
-  invitation = Invitation.new(email: @user.email, sender_id: current_user.id, recipient_id: params[:user_id] )
-  redirect_to user_path(@user)
+    @group = Group.find_by(id: params[:group_id])
+    @invitation = Invitation.new(invitation_params)
+      if @invitation.save
+        InvitationMailer.invitation_email(@invitation).deliver_now
+        redirect_to group_invitation_path(@group, @invitation)
+      else
+        format.html { render action: 'new'}
+      end
+  end
+
+  private
+  def invitation_params
+    params.require(:invitation).permit(:sender_id, :recipient_id)
   end
 
 end
