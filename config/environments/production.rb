527096824d3d782f75656cf7abc14561cd0d4ab4
Al Musavi & Sarah Dudek <pair+almusavi.sarahdudek@devbootcamp.com>
2017-03-19 15:15:15 -0500

    add email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
 # SMTP settings for gmail
-config.action_mailer.smtp_settings = {
+config.action_mailer.sendmail_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
  :user_name            => ENV['gmail_username'],
