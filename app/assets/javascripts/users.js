9ce991676f7e7df128420f28cbfa52401af4689d
Noelle Barber <barberossa@gmail.com>
2017-03-19 15:13:11 -0500

    user that creates group is the first member

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,20 @@
+var app window.app = {};
+
+app.Users = function(){
+  this._input = $('.friend-search');
+  this._initAutocomplete();
+};
+
+app.Users.prototype {
+
+};
+
+_initAutocomplete: function(){
+  this._input
+  .autocomplete({
+    source: '/users'
+    appendTo: '.friend-search'
+    select: $.prooxy(this._select, this)
+  })
+  .autocomplete('instance')._renderItem = $.prox(this._render, this);
+}
