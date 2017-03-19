707c72b4353fa32bb145450badd72810738b11ca
Al Musavi & Sarah Dudek <pair+almusavi.sarahdudek@devbootcamp.com>
2017-03-19 15:39:23 -0500

    add mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,9 +35,7 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :sendmail
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
