3167a580eabf63e68b7b5adb39e963fae6837879
Noelle Barber <barberossa@gmail.com>
2017-03-19 12:13:56 -0500

    lists the unpaid memberships on the users page

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,11 +4,16 @@ class InvitationsController < ApplicationController
   end
 
   def create
-  p params
-  p current_user
-  @user = User.find_by(id: params[:user_id])
-  invitation = Invitation.new(email: @user.email, sender_id: current_user.id, recipient_id: params[:user_id] )
-  redirect_to user_path(@user)
+    p params
+    p current_user
+    @user = User.find_by(id: params[:user_id])
+    invitation = Invitation.new(email: @user.email, sender_id: current_user.id, recipient_id: params[:user_id] )
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find_by(id: params[:user_id])
+    @invitations = @user.invitations
   end
 
 end
