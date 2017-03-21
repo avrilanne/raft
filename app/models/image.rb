3697a47eb6fe0cae0364f3b5b67d527b1df68817
Sarah Dudek <email": null>
2017-03-21 15:45:21 -0500

    Fixed carousel

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
 
-  has_attached_file :group_image, styles: { thumb: "100x100#"}, default_url: "thumb/missing.png"
+  has_attached_file :group_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "thumb/missing.png"
   validates_attachment_content_type :group_image, content_type: /\Aimage\/.*\z/
   belongs_to :group
 
