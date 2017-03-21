c4960d9e1a7cad0fd9d4f76f90e233702654e2f8
Noelle Barber & Sarah Dudek <pair+noelledusahel.sarahdudek@devbootcamp.com>
2017-03-21 18:19:41 -0500

    edit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,6 +5,3 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Rails.application.config.secret_token = 'R2PsejEjO8PwYbmB6lqQwR7jz64USwY7QYeoFcFgocdKcG2XUe'
-
-
-https://www.dwolla.com/oauth/v2/authenticate?client_id=1C0twahzLpOtJuc8UVNqDys3NSeCXABTDTbEUyJKUxUFV7Kxwf&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2Freturn&response_type=code&scope=accountinfofull%7Cbalance%7Csend
