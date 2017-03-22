b750a80b156b83f6994a8d7cb3328627923f2d02
Sarah Dudek <email": null>
2017-03-21 22:11:18 -0500

    Refactor and styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_attached_file :avatar, styles: { thumb: "100x100#"}, default_url: "thumb/missing.png"
+  has_attached_file :avatar, styles: {tiny:"50x50#", thumb: "100x100#"}, default_url: "thumb/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   has_merit
