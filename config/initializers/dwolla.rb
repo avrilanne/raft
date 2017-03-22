cf9d7e7cdd7cc8c70f9530b8ea93f19738792311
Noelle Barber & Sarah Dudek <pair+noelledusahel.sarahdudek@devbootcamp.com>
2017-03-22 10:04:00 -0500

    remove stuff

--- config/initializers/dwolla.rb
+++ config/initializers/dwolla.rb
@@ -1,7 +1,7 @@
 
-Dwolla::api_key = ENV['DWOLLA_APP_KEY']
-Dwolla::api_secret = ENV['DWOLLA_APP_SECRET']
-Dwolla::sandbox = true
+# Dwolla::api_key = ENV['DWOLLA_APP_KEY']
+# Dwolla::api_secret = ENV['DWOLLA_APP_SECRET']
+# Dwolla::sandbox = true
 
 $dwolla = DwollaV2::Client.new(key: ENV['DWOLLA_APP_KEY'], secret: ENV['DWOLLA_APP_SECRET']) do |config|
     config.environment = :sandbox
